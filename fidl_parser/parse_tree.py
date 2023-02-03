017da45db92bc1088c150780a0e3b5b8248c5678
Gunnar Andersson <gunnar_dev@nospam@novaspring.eu>
2023-02-03 14:22:31 +0100

    Change debug-printouts to a dedicated function
    It differentiates logs (in this case "debug"-printouts) from necessary
    printouts and enables a single place to disable only those.

--- fidl_parser/parse_tree.py
+++ fidl_parser/parse_tree.py
@@ -8,6 +8,11 @@ import lark
 import json
 from . import type_manager
 
+# Debug printout wrapper to have a single place to turn them off
+def log(s):
+    #print(s)
+    pass
+
 indent_level = 0
 func_stack = []
 def indent(func):
@@ -54,29 +59,29 @@ def process_lark_tree(state, lark_tree, parse_map):
 @indent
 def process_lark_tree_entry(state, lark_tree, lark_entry, parse_map):
     # Locate the dictionary entry we want to process
-    print(f"{ind()}Called for {lark_tree.data}[{lark_entry}]")
+    log(f"{ind()}Called for {lark_tree.data}[{lark_entry}]")
 
     entries = get_lark_tree_entries_by_name(state, lark_tree, lark_entry)
     if entries == []:
-        print(f"{ind()}No match for {lark_entry}. Return False.")
+        log(f"{ind()}No match for {lark_entry}. Return False.")
         return None
 
     if len(entries) > 1:
-        print(f"{ind()}Too many matches, {len(entries)} in {lark_entry}. exit.")
+        log(f"{ind()}Too many matches, {len(entries)} in {lark_entry}. exit.")
         raise Exception(f"Too many matches for {lark_tree.data}[{lark_entry}]-> {len(entries)}")
 
     result = process_lark_tree(state, entries[0], parse_map)
-    print(f"{ind()}Result: {type(result)} - {len(result)} elements")
+    log(f"{ind()}Result: {type(result)} - {len(result)} elements")
     return result
 
 @indent
 def create_entry(state, lark_tree, lark_type, target_name, target_type=str):
     res = get_lark_tree_token_by_type(state, lark_tree, lark_type)
     if not res:
-        print(f"{ind()}Not found: {lark_type}")
+        log(f"{ind()}Not found: {lark_type}")
         return None
 
-    print(f"{ind()}Found {lark_type} = {res} -> {target_name}")
+    log(f"{ind()}Found {lark_type} = {res} -> {target_name}")
     if target_type == int and res.value=="minInt":
         res.value = -9223372036854775808
 
@@ -88,19 +93,19 @@ def create_entry(state, lark_tree, lark_type, target_name, target_type=str):
 
 @indent
 def create_target_dictionary(state, lark_tree, lark_entry, target_entry, parse_map):
-    print(f"{ind()}Called for {lark_tree.data}[{lark_entry}] -> {target_entry}")
+    log(f"{ind()}Called for {lark_tree.data}[{lark_entry}] -> {target_entry}")
     res = process_lark_tree_entry(state, lark_tree, lark_entry, parse_map)
     if not res:
-        print(f"{ind()}Got None back. No action.")
+        log(f"{ind()}Got None back. No action.")
         return None
 
-    print(f"{ind()}Create new {target_entry} dictionary with: {type(res)} - {len(res)} elements.")
+    log(f"{ind()}Create new {target_entry} dictionary with: {type(res)} - {len(res)} elements.")
     return { target_entry: res }
 
 
 @indent
 def create_target_list(state, lark_tree, lark_entry, target_entry, parse_map):
-    print(f"{ind()}Called for {lark_tree.data}[{lark_entry}] -> {target_entry}")
+    log(f"{ind()}Called for {lark_tree.data}[{lark_entry}] -> {target_entry}")
     entries = get_lark_tree_entries_by_name(state, lark_tree, lark_entry)
     if entries == []:
         return None
@@ -123,7 +128,7 @@ def aggregate_list(state, lark_tree, element_name, parse_map):
         if res and element_name in res:
             result.extend(res[element_name])
 
-    print(f"{ind()}Will aggregate {len(result)} elements into {element_name}")
+    log(f"{ind()}Will aggregate {len(result)} elements into {element_name}")
     return { element_name: result }
 
 @indent
@@ -145,9 +150,9 @@ def create_entry_from_type_token(state, lark_tree, target_entry):
         'FIDL_BYTEBUFFER': 'binary'
     }
 
-    print(f"{ind()}Called for {lark_tree.data} -> {target_entry}")
+    log(f"{ind()}Called for {lark_tree.data} -> {target_entry}")
     if len(lark_tree.children) == 0:
-        print(f"{ind()}{lark_tree.data} has no tokens. No action")
+        log(f"{ind()}{lark_tree.data} has no tokens. No action")
         return None
 
     if len(lark_tree.children) > 1:
@@ -162,7 +167,7 @@ def create_entry_from_type_token(state, lark_tree, target_entry):
 
 @indent
 def process_one_of(state, lark_tree, options):
-    print(f"{ind()}Called for {lark_tree.data}")
+    log(f"{ind()}Called for {lark_tree.data}")
     for (map_func, *arg) in options:
         res = map_func(state, lark_tree, *arg)
         if res:
@@ -202,7 +207,7 @@ def add_datatype(state, lark_elem, parse_map):
     # Add datatype to symbol table for current
     # namestate
 
-    print(f"{ind()}ADDING DATATYPE---------------- {res['name']}")
+    log(f"{ind()}ADDING DATATYPE---------------- {res['name']}")
 
     state['ns'].add_type(type_manager.Type(res['name'], res))
 
@@ -214,7 +219,7 @@ def resolve_datatypes(state, lark_tree, parse_map):
     def _resolve_datatype(dict_tree):
         resolved_result = None
         for k, v in dict_tree.items():
-            print(f"{ind()} Checking {k}")
+            log(f"{ind()} Checking {k}")
 
             # Is this the already resolved datatype?
             if k == '_resolved_datatype':
@@ -286,49 +291,49 @@ def evaluate_expression(state, lark_tree, expr_name, target_name):
 
     @indent
     def eval_token(expr_token):
-        print(f"{ind()}Evaluating {expr_token}")
+        log(f"{ind()}Evaluating {expr_token}")
 
 
     @indent
     def eval_tree(expr_tree):
-        print(f"{ind()}Evaluting {expr_tree.data}")
+        log(f"{ind()}Evaluting {expr_tree.data}")
 
         for expr in expr_tree.children:
-            print(f"{ind()}Checking {expr}")
+            log(f"{ind()}Checking {expr}")
             if isinstance(expr, lark.Token):
                 if expr.type in token_map:
-                    print(f"{ind()}Evaluating token {expr.type} = {expr.value}")
+                    log(f"{ind()}Evaluating token {expr.type} = {expr.value}")
                     return token_map[expr.type](expr.value)
                 else:
-                    print(f"{ind()}No token lambda available to evaluate {expr.type}")
+                    log(f"{ind()}No token lambda available to evaluate {expr.type}")
                     continue
             else:
                 if expr.data in tree_map:
-                    print(f"{ind()}Evaluating tree {expr.data}")
+                    log(f"{ind()}Evaluating tree {expr.data}")
                     return tree_map[expr.data](expr)
                 else:
-                    print(f"{ind()}No tree lambda available to evaluate {expr.data}")
+                    log(f"{ind()}No tree lambda available to evaluate {expr.data}")
                     continue
 
-        print(f"{ind()}No hit")
+        log(f"{ind()}No hit")
         return None
 
     @indent
     def eval_expression(expr_tree, expr_name):
-        print(f"{ind()}Searching for {expr_name} in {expr_tree.data}")
+        log(f"{ind()}Searching for {expr_name} in {expr_tree.data}")
 
         for expr in expr_tree.children:
-            print(f"{ind()}Checking {expr}")
+            log(f"{ind()}Checking {expr}")
             if not isinstance(expr, lark.Tree) or expr.data != expr_name:
-                print(f"{ind()}Nope")
+                log(f"{ind()}Nope")
                 continue
 
             return eval_tree(expr)
 
-        print(f"{ind()}No hit")
+        log(f"{ind()}No hit")
         return None
 
-    print(f"{ind()}Evaluating {lark_tree.data}/{expr_name}")
+    log(f"{ind()}Evaluating {lark_tree.data}/{expr_name}")
     return { target_name: tree_map[expr_name](lark_tree) }
 
 
@@ -483,10 +488,10 @@ def resolve_types(current_namespace, name, tree):
     if not isinstance(tree, dict):
         return None
 
-    print(f"{ind()}Resolving {name}")
+    log(f"{ind()}Resolving {name}")
 
     if 'datatype' in tree:
-        print(f"{ind()}Found datatype")
+        log(f"{ind()}Found datatype")
 
         res = current_namespace.resolve_type(f"{tree['datatype']}")
         if not res:
@@ -496,7 +501,7 @@ def resolve_types(current_namespace, name, tree):
                 raise Exception(f"Could not resolve type name {tree['datatype']}")
 
         tree['$resolved_datatype'] = res
-        print(f"{ind()}Resolved to: {res}")
+        log(f"{ind()}Resolved to: {res}")
 
     # Traverse namespaces and recursively resolve
     if 'namespaces' in tree:
@@ -505,7 +510,7 @@ def resolve_types(current_namespace, name, tree):
 
     # Check for any lists to traverse:
     for k, v in tree.items():
-        print(f"{ind()}Checking {k}")
+        log(f"{ind()}Checking {k}")
         # Filter out stuff we don't need
         if k == 'namespaces' or k =='datatype':
             continue
